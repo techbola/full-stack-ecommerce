f8e6b8490c05e5b91cb5e5d04929c600eb763fb5
Adebola <adebola@poptin.com>
2024-06-20 09:55:37 +0100

    disable add to cart button for product already in cart

--- src/pages/ProductDetailPage.vue
+++ src/pages/ProductDetailPage.vue
@@ -6,7 +6,10 @@
     <div class="product-details">
       <h1>{{ product.name }}</h1>
       <h3 class="price">{{ product.price }}</h3>
-      <button @click="addToCart()" class="add-to-cart">Add to cart</button>
+      <button @click="addToCart()" :class="itemIsInCart ? 'grey-button' : 'add-to-cart'"
+        :disabled="itemIsInCart ? true : false">Add
+        to
+        cart</button>
     </div>
   </div>
   <div v-else>
@@ -26,18 +29,31 @@ export default {
   data() {
     return {
       product: {},
+      cartItems: [],
     }
   },
   methods: {
     async addToCart() {
       await axios.post('/api/users/12345/cart', { id: this.$route.params.productId });
       alert('Product added to cart!');
+    },
+    async getCartItems() {
+      const response = await axios.get('/api/users/12345/cart/');
+      const cartItems = response.data;
+      this.cartItems = cartItems;
+    }
+  },
+  computed: {
+    itemIsInCart() {
+      return this.cartItems.some(item => item.id === this.$route.params.productId);
     }
   },
   async created() {
     const response = await axios.get(`/api/products/${this.$route.params.productId}`);
     const product = response.data;
     this.product = product;
+
+    this.getCartItems();
   },
 }
 </script>
